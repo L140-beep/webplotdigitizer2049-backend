faefd8a920331f2b886e499c028dedd39e3d96e0
L140-beep <70793854+L140-beep@users.noreply.github.com>
2022-07-03 15:51:11 +0800

    function tup_rgb added

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import numpy as np
 import json
 import plotdigitizer
 
+def tup_rgb(rgb: str):
+    return tuple(rgb[4:-1].replace(',', ' ').split())
+
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-type'
@@ -22,13 +25,13 @@ def index():
     img = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
     
     if(graph_type == 'line'):
-        return json.dumps({"dataset" :plotdigitizer.line(img, color[0], dots)})
+        return json.dumps({"dataset" :plotdigitizer.line(img, tup_rgb(color[0]), dots)})
     elif(graph_type == 'points'):
-        return json.dumps({"dataset" :plotdigitizer.points(img, color[0], dots)})
+        return json.dumps({"dataset" :plotdigitizer.points(img, tup_rgb(color[0]), dots)})
     elif(graph_type == 'line_filled'):
-        return json.dumps({"dataset" :plotdigitizer.line_filled(img, color[0], dots)})
+        return json.dumps({"dataset" :plotdigitizer.line_filled(img, tup_rgb(color[0]), dots)})
     elif(graph_type == 'barplot'):
-        return json.dumps({"dataset" : plotdigitizer.barplot(img, color[0], dots)})
+        return json.dumps({"dataset" : plotdigitizer.barplot(img, tup_rgb(color[0]), dots)})
     else:
         return('NOT SUPPORTED')
 @app.route('/', methods=["GET"])
